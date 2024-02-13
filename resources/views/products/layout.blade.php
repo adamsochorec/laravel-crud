167a453ec7b2066fe13054d786f14a2642b3fd4b
Adam S <98590550+adamsochorec@users.noreply.github.com>
2024-02-14 00:21:55 +0100

    Creating first views

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Laravel Crud Application</title>
+        <link
+            href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css"
+            rel="stylesheet"
+        />
+    </head>
+    <body>
+        <h1>Laravel Crud Application</h1>
+        <div class="container">
+            <br />
+            @yield('content')
+        </div>
+    </body>
+</html>
